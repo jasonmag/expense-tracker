1d56cfb38cce1c2074a59dd62ba0c78ab27f0492
JasonMag <jasonmaglangit@gmail.com>
2023-07-05 23:44:17 +0800

    updated form for new expenses

--- src/pages/NewExpense.js
+++ src/pages/NewExpense.js
@@ -1,31 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // Define the constant variable here
 const API_URL = "http://localhost:3001/api/expenses";
+const CATEGORY_API_URL = "http://localhost:3001/api/categories";
+const authToken = localStorage.getItem('authToken');
 
 export const NewExpense = () => {
   const [amount, setAmount] = useState('');
   const [category, setCategory] = useState('');
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+
+    // Fetch the categories from the API when the component mounts
+    axios.get(CATEGORY_API_URL, {
+      headers: {
+        Authorization: `${authToken}`,
+      },
+    })
+      .then((response) => {
+        setCategories(response.data.categories);
+        // Set the default category as the first category returned from the API
+        if (response.data.categories.length > 0) {
+          setCategory(response.data.categories[0].id);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Make the POST request here as well (in case you want to submit the form via button click)
+    // Make the POST request with the authToken and category in the headers
     axios.post(API_URL, {
       amount: amount,
-      category: category,
+      category_id: category,
       date: date,
       description: description,
+    }, {
+      headers: {
+        Authorization: `${authToken}`,
+      },
     })
     .then((response) => {
       // Handle the response if needed
       console.log("Response:", response.data);
       // Clear the form after successful submission
       setAmount('');
-      setCategory('');
+      // Set the default category as the first category returned from the API
+      if (categories.length > 0) {
+        setCategory(categories[0].id);
+      }
       setDate('');
       setDescription('');
     })
@@ -35,16 +65,13 @@ export const NewExpense = () => {
     });
   };
 
-
   return (
     <div className="px-4">
       <form onSubmit={handleSubmit}>
         <div className="space-y-12">
-          {/* Existing code */}
-        
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">New Expense</h2>
-      
+
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
                 <label htmlFor="amount" className="block text-sm font-medium leading-6 text-gray-900">
@@ -55,29 +82,34 @@ export const NewExpense = () => {
                     type="text"
                     name="amount"
                     id="amount"
-                    value={amount} // Assign the state value to the input field
-                    onChange={(e) => setAmount(e.target.value)} // Update the state when the input changes
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-      
+
               <div className="sm:col-span-2">
                 <label htmlFor="category" className="block text-sm font-medium leading-6 text-gray-900">
                   Category
                 </label>
                 <div className="mt-2">
-                  <input
-                    type="text"
+                  <select
                     name="category"
                     id="category"
-                    value={category} // Assign the state value to the input field
-                    onChange={(e) => setCategory(e.target.value)} // Update the state when the input changes
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+                  >
+                    {categories.map((cat) => (
+                      <option key={cat.id} value={cat.id}>
+                        {cat.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
-      
+
               <div className="sm:col-span-2">
                 <label htmlFor="date" className="block text-sm font-medium leading-6 text-gray-900">
                   Date
@@ -87,13 +119,13 @@ export const NewExpense = () => {
                     type="date"
                     name="date"
                     id="date"
-                    value={date} // Assign the state value to the input field
-                    onChange={(e) => setDate(e.target.value)} // Update the state when the input changes
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-      
+
               <div className="col-span-full">
                 <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
                   Description
@@ -103,17 +135,14 @@ export const NewExpense = () => {
                     id="description"
                     name="description"
                     rows={3}
-                    value={description} // Assign the state value to the textarea
-                    onChange={(e) => setDescription(e.target.value)} // Update the state when the textarea changes
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={''}
                   />
                 </div>
               </div>
             </div>
           </div>
-      
-          {/* Existing code */}
         </div>
         <div className="py-4">
           <button
