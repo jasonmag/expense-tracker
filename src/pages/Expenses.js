0e38f144dc2148e0aeaa7648d512aec95ca962c4
JasonMag <jasonmaglangit@gmail.com>
2023-07-05 23:56:32 +0800

    updated arrangement for expenses

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 const apiUrl = 'http://localhost:3001/api/expenses';
+const categoriesApiUrl = 'http://localhost:3001/api/categories';
 
 export function Expenses() {
   const [expenses, setExpenses] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [categories, setCategories] = useState({});
 
   useEffect(() => {
     async function fetchExpenses() {
@@ -34,8 +36,37 @@ export function Expenses() {
       }
     }
 
+    async function fetchCategories() {
+      try {
+        const authToken = localStorage.getItem('authToken');
+        if (!authToken) {
+          throw new Error('Authentication token not found');
+        }
+
+        const response = await fetch(categoriesApiUrl, {
+          headers: {
+            Authorization: `${authToken}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        const categoryData = data.categories.reduce((acc, category) => {
+          acc[category.id] = category.name;
+          return acc;
+        }, {});
+        setCategories(categoryData);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
     fetchExpenses();
-  }, []); // Empty dependency array, so useEffect runs only once when the component mounts
+    fetchCategories();
+  }, []);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -50,19 +81,19 @@ export function Expenses() {
       <table className="w-full divide-y divide-gray-100">
         <thead>
           <tr>
-            <th className="text-center">Description</th>
             <th className="text-center">Date</th>
+            <th className="text-left">Category</th>
+            <th className="text-left">Description</th>
             <th className="text-center">Amount</th>
-            <th className="text-center">Category ID</th>
           </tr>
         </thead>
         <tbody>
           {expenses.map((expense, index) => (
             <tr key={index}>
-              <td className="text-center">{expense.description}</td>
               <td className="text-center">{expense.date}</td>
-              <td className="text-center">${expense.amount}</td>
-              <td className="text-center">{expense.category_id}</td>
+              <td className="text-left">{categories[expense.category_id]}</td>
+              <td className="text-left">{expense.description}</td>
+              <td className="text-right">${expense.amount}</td>
             </tr>
           ))}
         </tbody>
