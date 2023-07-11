7597451d724d722e663831fca3198b6459d936e3
JasonMag <jasonmaglangit@gmail.com>
2023-07-12 00:20:21 +0800

    updated new expense form with updated fields

--- src/pages/NewExpense.js
+++ src/pages/NewExpense.js
@@ -1,68 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-// Define the constant variable here
 const API_URL = "http://localhost:3001/api/expenses";
-const CATEGORY_API_URL = "http://localhost:3001/api/categories";
+//TODO Add Accounts, Transaction Types
 const authToken = localStorage.getItem('authToken');
+
 
 export const NewExpense = () => {
   const [amount, setAmount] = useState('');
-  const [category, setCategory] = useState('');
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-
-    // Fetch the categories from the API when the component mounts
-    axios.get(CATEGORY_API_URL, {
-      headers: {
-        Authorization: `${authToken}`,
-      },
-    })
-      .then((response) => {
-        setCategories(response.data.categories);
-        // Set the default category as the first category returned from the API
-        if (response.data.categories.length > 0) {
-          setCategory(response.data.categories[0].id);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching categories:", error);
-      });
-  }, []);
+  const [acctBy, setAcctBy] = useState('');
+  const [acctTo, setAcctTo] = useState('');
+  const [transactionTypeId, setTransactionTypeId] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Make the POST request with the authToken and category in the headers
     axios.post(API_URL, {
       amount: amount,
-      category_id: category,
+      acct_by: acctBy,
+      acct_to: acctTo,
       date: date,
       description: description,
+      transaction_type_id: transactionTypeId,
     }, {
       headers: {
         Authorization: `${authToken}`,
       },
     })
-    .then((response) => {
-      // Handle the response if needed
-      console.log("Response:", response.data);
-      // Clear the form after successful submission
-      setAmount('');
-      // Set the default category as the first category returned from the API
-      if (categories.length > 0) {
-        setCategory(categories[0].id);
-      }
-      setDate('');
-      setDescription('');
-    })
-    .catch((error) => {
-      // Handle errors if any
-      console.error("Error:", error);
-    });
+      .then((response) => {
+        console.log("Response:", response.data);
+        setAmount('');
+        setDate('');
+        setDescription('');
+        setAcctBy('');
+        setAcctTo('');
+        setTransactionTypeId('');
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
@@ -90,27 +68,6 @@ export const NewExpense = () => {
               </div>
 
               <div className="sm:col-span-2">
-                <label htmlFor="category" className="block text-sm font-medium leading-6 text-gray-900">
-                  Category
-                </label>
-                <div className="mt-2">
-                  <select
-                    name="category"
-                    id="category"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  >
-                    {categories.map((cat) => (
-                      <option key={cat.id} value={cat.id}>
-                        {cat.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
                 <label htmlFor="date" className="block text-sm font-medium leading-6 text-gray-900">
                   Date
                 </label>
@@ -121,6 +78,54 @@ export const NewExpense = () => {
                     id="date"
                     value={date}
                     onChange={(e) => setDate(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="acct_by" className="block text-sm font-medium leading-6 text-gray-900">
+                  Account By
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="acct_by"
+                    id="acct_by"
+                    value={acctBy}
+                    onChange={(e) => setAcctBy(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="acct_to" className="block text-sm font-medium leading-6 text-gray-900">
+                  Account To
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="acct_to"
+                    id="acct_to"
+                    value={acctTo}
+                    onChange={(e) => setAcctTo(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="transaction_type_id" className="block text-sm font-medium leading-6 text-gray-900">
+                  Transaction Type ID
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="transaction_type_id"
+                    id="transaction_type_id"
+                    value={transactionTypeId}
+                    onChange={(e) => setTransactionTypeId(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
