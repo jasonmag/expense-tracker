68bf597b9886641c3eceb406e71cc9bca8249674
JasonMag <jasonmaglangit@gmail.com>
2023-07-25 23:51:35 +0800

    added class style to button

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ButtonLink = ({ to, text }) => {
   return (
-    <Link to={to} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <Link to={to} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">
       {text}
     </Link>
   );
